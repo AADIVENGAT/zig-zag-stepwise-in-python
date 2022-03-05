4b2fb8eec624d433d290bcdbcc340683868583e2
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-05 07:06:49 +0530

    Add files via upload

---
+++ march3.14.py
@@ -0,0 +1,24 @@
+# zig-zag step-wise shape:
+from turtle import*
+begin_fill
+color("red")
+
+backward(200)
+left(50)
+forward(200)
+right(50)
+backward(200)
+left(50)
+forward(200)
+right(50)
+
+backward(200)
+left(50)
+forward(200)
+right(50)
+backward(200)
+left(50)
+forward(200)
+right(50)
+
+end_fill 
